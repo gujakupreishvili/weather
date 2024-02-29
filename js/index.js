e1f391e07a4aa43a23387460ca3e6349eb8c0615
gujakupreishvili <gujakupreishvili4@gmail.com>
2024-02-29 12:37:13 +0400

    make respons

--- js/index.js
+++ js/index.js
@@ -12,42 +12,57 @@ const cloud = document.querySelector(".cloud");
 const wind_speed = document.querySelector(".wind-speed-span");
 
 const proxyUrl = "https://api.allorigins.win/raw?url="; // Use AllOrigins as the proxy
+function check_weather() {
+  fetch(
+    `${proxyUrl}https://lobster-app-kps4x.ondigitalocean.app/api/weather/${input.value}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      temp.textContent = data.currentTemp;
+      city.textContent = data.city;
+      week_day.textContent = data.day;
+      hour.textContent = formattedTime;
+      data.textContent = get_data;
+      month.textContent = get_month;
+      max_temp.textContent = data.maxTemp;
+      min_temp.textContent = data.minTemp;
+      humadity.textContent = data.humidity;
+      wind_speed.textContent = data.windSpeed;
+      if (data.currentWeatherDesc === "sunny") {
+        document.body.style.backgroundImage = `url('../img/thunder.png')`;
+      }
+      if (data.currentWeatherDesc === "rain") {
+        document.body.style.backgroundImage = `url('../img/rain.png')`;
+      }
+      if (data.currentWeatherDesc === "snowy") {
+        document.body.style.backgroundImage = `url('../img/snow.png')`;
+      }
+    })
+
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+      const input_form = document.querySelector(".input-form");
+      input_form.style.borderBottom = "1px solid red";
+    });
+  input.value = "";
+}
+
+// window.addEventListener("load", () => {
+//   const loader = document.querySelector(".loader");
+//   loader.classList.add("loader-hidden");
+//   loader.addEventListener("transitionend", () => {
+//     document.body.removeChild("loader");
+//   });
+// });
 
 input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    if (input.value === "") {
-      alert(error);
-    }
     e.preventDefault();
-    fetch(
-      `${proxyUrl}https://lobster-app-kps4x.ondigitalocean.app/api/weather/${input.value}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        temp.textContent = data.currentTemp;
-        city.textContent = data.city;
-        week_day.textContent = data.day;
-        hour.textContent = formattedTime;
-        data.textContent = get_data;
-        month.textContent = get_month;
-        max_temp.textContent = data.maxTemp;
-        min_temp.textContent = data.minTemp;
-        humadity.textContent = data.humidity;
-        wind_speed.textContent = data.windSpeed;
-        if (data.currentWeatherDesc === "sunny") {
-          document.body.style.backgroundImage = `url('../img/sun.jpg')`;
-          document.body.style.backgroundSize = "cover"; // or "contain", depending on your needs
-          document.body.style.maxWidth = "740px";
-          document.body.style.minHeight = "400px"; // Set the maximum height here
-          document.body.style.margin = "0 auto"; // Center the background image horizontally
-        }
-      })
-
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-    input.value = "";
+    // const loader = document.querySelector(".loader");
+    check_weather();
+    // loader.classList.remove("loader-hidden");
+    // loader.style.display = "none";
   }
 });
 
@@ -71,3 +86,4 @@ const months = [
 ];
 const get_month = months[time.getMonth()];
 const formattedTime = `${hours}:${minutes}`;
+const input_desktop = document.querySelector(".input-desktop");
